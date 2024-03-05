6f7fd7ed5fa74aeab4f9a668317b07dddeb0f6cd
Galangputra123 <pgalang785@amikom.ac.id>
2024-03-05 12:08:28 +0700

    Menambahkan kode

---
+++ Calculator.cs
@@ -0,0 +1,16 @@
+class Calculator
+{
+     static void mian(string[] args)
+     {
+        
+	 }
+	 static int penambahan(int a, int b)
+	 {
+		 return a + b;
+	 }
+	 static int pengurangan(int a, b)
+	 {
+		 retun a - b;
+	 }
+	 
+}	 
